fd3445ced16ee155705ff915ccdcbcf02c4bcb54
Vyacheslav <ResSlavkaRes@mail.ru>
2022-10-07 13:48:07 +0300

    init2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const tg = window.Telegram.WebApp;
 function App() {
 
 
-    useEffect(() => {
-        tg.ready();
-    }, [])
+    // useEffect(() => {
+    //     tg.ready();
+    // }, [])
 
     const onClose = () => {
         tg.close()
